fe197e1689d07227bb5652df46a4e9957e0a48be
Luca Raveri <lucaraveri993@gmail.com>
2024-10-12 17:17:46 +0200

    feat: add secret manager

--- handler.js
+++ handler.js
@@ -1,11 +1,24 @@
 'use strict';
 
 const AWS = require('aws-sdk');
+const secretsManager = new AWS.SecretsManager();
+
+const getWebhookSecret = async () => {
+  const params = {
+    SecretId: "webhook-signature"
+  };
+  const secretValue = await secretsManager.getSecretValue(params).promise();
+
+  return JSON.parse(secretValue.SecretString)["webhook-signature"];
+};
 
 module.exports.webhook = async (event) => {
   console.log('Received Webhook Event:', event);
 
   try {
+    const webhookSecret = process.env.WEBHOOK_SIGNATURE;
+    console.log('Webhook Secret:', webhookSecret);
+
     // Log the body of the event (webhook data)
     const body = JSON.parse(event.body);
     console.log('Webhook Body:', body);
@@ -14,11 +27,19 @@ module.exports.webhook = async (event) => {
     console.log('Webhook Headers:', event.headers);
 
     // Additional logic goes here (e.g., authentication, saving to DynamoDB)
+    if(event.headers['signature'] !== webhookSecret) {
+        return {
+          statusCode: 401,
+          body: JSON.stringify({
+            message: 'Unauthorized',
+          }),
+        };
+    }
 
     return {
       statusCode: 200,
       body: JSON.stringify({
-        message: 'Webhook received and processed!',
+        message: 'Webhook received and processed! ' + webhookSecret,
       }),
     };
   } catch (error) {
